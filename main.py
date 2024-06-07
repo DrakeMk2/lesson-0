4c69f5c01caab54ba4efc8d7965cec164b0c4fce
DrakeMk2 <doosiv@gmail.com>
2024-06-07 15:11:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a="Anga-Banga"
+print('#1',len(a),'=))')
+
+first=42
+second=7
+summa=first+second
+diff=first-second
+print('#2',summa,diff,'=))')
+
+first=42
+second=7
+third=12
+mean=(first+second+third)/3  # Через функцию mean или fmean не получилось. Писал в ошибке что не понимает таких.
+print('#3',mean)
+
+first_string='Вторник'
+second_string='Понедельник'
+print('#4',second_string+', '+first_string,'=))')
+
+a=42
+b=7
+c=12
+f=((((a*b)+(a*c))**3)/2)
+print('#5',f,'=))')
